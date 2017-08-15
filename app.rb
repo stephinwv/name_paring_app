c41518a1ce4c5399ca6389ac54e56cc07ec9f212
stephinwv <thekingdomseeker@aol.com>
2017-08-14 23:39:07 -0400

    Add files via upload
    Cleaned up unnecessary bits of gets and posts from app.rb, and added minimal CSS.

--- app.rb
+++ app.rb
@@ -4,12 +4,7 @@ require_relative 'random_pairing.rb'
 get "/" do
 	erb :index
 end
-post '/index' do
-	
-	number = params[:number]
-	text = params[:text]
-	# redirect'/names?number='+number+'&text='+text
-end
+
 get '/names'do
 	names = params.values
 	pair = pair(names)
@@ -18,7 +13,7 @@ get '/names'do
 end
 post '/names' do
 	puts "MADE IT TO POST NAMES"
-	names = params[:names]
+	
 	names = params.values
 	pair = pair(names)
 erb :names, :locals=>{:pair=> pair}
